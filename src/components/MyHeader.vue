ed3a74ca602485e6c951b2374fa95af66cefe067
Holly Emma Johnson <hollyjnz@gmail.com>
2019-03-12 19:05:01 +1300

    geoff's screen

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,16 +1,24 @@
 <template>  
-  <nav><p>test</p></nav> 
+  <nav>
+    <ul>
+      <li><router-link v-bind:to="'/'" exact>Home</router-link></li>
+      <li><router-link v-bind:to="'/geoff'" exact>Geoff's Screen</router-link></li>
+    </ul>
+  </nav> 
 </template>
 
 <script>
 export default {
-  name: "MyHeader",
-  props: {
-    msg: String
-  }
+  name: "MyHeader"
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-evenly;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
 </style>
---
+++ src/components/geoff/Geoff.vue
@@ -0,0 +1,17 @@
+<template>  
+    <h1>hello</h1>
+</template>
+
+<script>
+export default {
+  name: "Geoff"
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h1 {
+        font-size: 10rem;
+        font-family: "Comic Sans MS", cursive, sans-serif;
+    }
+</style>
