af908a513fe4ebc45805b9f0cd65978d0859baa1
Geoffrey philip james Sargison <932400987@dov2-sc-28.local>
2019-03-12 16:57:55 +1300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <MyHeader/>
+    <router-view></router-view >
+  </div>
+</template>
+
+<script>
+import MyHeader from "./components/MyHeader";
+
+export default {
+  name: "App",
+  components: {
+    MyHeader
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,16 @@
+<template>  
+  <nav><p>test</p></nav> 
+</template>
+
+<script>
+export default {
+  name: "MyHeader",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
